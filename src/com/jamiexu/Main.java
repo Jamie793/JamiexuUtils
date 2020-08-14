512f3befd5720318c5b2fe84f96e10dfb31d868b
Jamie793 <doxm@foxmial.com>
2020-08-14 12:37:00 +0800

    last update: 2020/8/14 12:36 Jamiexu/Jamie793,update content: Overrid write HttpUtils

--- src/com/jamiexu/Main.java
+++ src/com/jamiexu/Main.java
@@ -1,6 +1,7 @@
 package com.jamiexu;
 
 //import com.jamiexu.utils.convert.ConvertUtils;
+
 import com.jamiexu.utils.encryption.EncryptionUtils;
 import com.jamiexu.utils.file.FileChannelUtils;
 import com.jamiexu.utils.file.FileUtils;
@@ -10,6 +11,8 @@ import com.jamiexu.utils.http.HttpUtils;
 import com.jamiexu.utils.http.IHttpListener;
 
 import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
@@ -22,49 +25,54 @@ import java.util.Objects;
  */
 
 public class Main {
+
+    public static int post = 0x00000001;
+    public static int download = 0x00000002;
+    public static int get = 0x00000004;
+    public static int upload = 0x00000008;
+
     public static void main(String[] args) {
-//        HttpUtils httpUtils = new HttpUtils("https://www.baidu.com");
-//        httpUtils.setHttpListener(new IHttpListener() {
-//            @Override
-//            public void onComplete(HttpResponse httpResponse) {
-//                System.out.println();
-//            }
-//
-//            @Override
-//            public void onError(Exception error) {
-//
-//            }
-//        });
-//        httpUtils.execute();;
-        deFile("C:\\Users\\1\\Desktop\\demo1");
+        new HttpUtils("http://127.0.0.1/test.php", "n=123", new IHttpListener() {
+            @Override
+            public void onComplete(HttpResponse httpResponse) {
+                System.out.println(httpResponse.getBody());
+            }
+
+            @Override
+            public void onError(Exception error) {
+                System.out.println(error);
+            }
+        }).setTimeout(1000).setDownloadPath("C:\\Users\\Jamiexu\\Desktop\\2.png")
+                .execute();
+
 
     }
 
-    public static void deFile(String path){
+    public static void deFile(String path) {
         File[] file = new File(path).listFiles();
-        for(File f:file){
-            if(f.isDirectory()){
+        for (File f : file) {
+            if (f.isDirectory()) {
                 deFile(f.toString());
-            }else if(f.getName().endsWith("aar")){
+            } else if (f.getName().endsWith("aar")) {
                 de(f.toString());
             }
         }
     }
 
 
-    public static void de(String path){
+    public static void de(String path) {
         FileInputStream fileInputStream = null;
         try {
             fileInputStream = new FileInputStream(path);
             byte[] bytes = new byte[fileInputStream.available()];
             fileInputStream.read(bytes);
             byte[] key = new byte[32];
-            System.arraycopy(bytes,bytes.length-32,key,0,32);
-            byte[] data = new byte[bytes.length-8-32];
-            System.arraycopy(bytes,8,data,0,bytes.length-8-32);
+            System.arraycopy(bytes, bytes.length - 32, key, 0, 32);
+            byte[] data = new byte[bytes.length - 8 - 32];
+            System.arraycopy(bytes, 8, data, 0, bytes.length - 8 - 32);
 //            System.out.println(new String(data));
-            data = en(data,key);
-            System.out.println(new String(data,0,data.length));
+            data = en(data, key);
+            System.out.println(new String(data, 0, data.length));
 //            FileUtils.writeFile(path.replace("aar",""),data);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -81,9 +89,9 @@ public class Main {
     }
 
     public static byte[] en(byte[] arrby, byte[] arrby2) {
-       for (int i=0;i<arrby.length;i++){
-           arrby[i] = (byte) (arrby[i] ^ 0xFF ^ arrby2[i%arrby2.length]);
-       }
-       return arrby;
+        for (int i = 0; i < arrby.length; i++) {
+            arrby[i] = (byte) (arrby[i] ^ 0xFF ^ arrby2[i % arrby2.length]);
+        }
+        return arrby;
     }
 }
--- src/com/jamiexu/utils/http/HttpUtils.java
+++ src/com/jamiexu/utils/http/HttpUtils.java
@@ -1,6 +1,8 @@
 package com.jamiexu.utils.http;
 
 
+import com.jamiexu.utils.file.FileUtils;
+
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.Proxy;
@@ -8,75 +10,145 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.zip.GZIPInputStream;
 
 /**
  * @author Jamiexu or Jamie793
- * @version 1.0
+ * @version 3.0
  * blog；blog.jamiexu.cn
  */
 
 public class HttpUtils {
+
+    private final int REQUEST_GET = 0x00000001;
+
+    private final int REQUEST_POST = 0x00000002;
+
+    private final int REQUEST_UPLOAD = 0x00000004;
+
+    private final int REQUEST_DOWNLOAD = 0x00000008;
+
+    private final int REQUEST_RAW = 0x000000010;
+
+    private final String REQUEST_LINE = "----979916206Jamiexu0Jamie7937401316330204";
+
+    private int requestMode = REQUEST_GET;
+
     private String url;
+
     private byte[] data = null;
+
     private HashMap<String, String> hashMap_headers;
+
     private IHttpListener iHttpListener;
+
     private int timeout = 6 * 1000;
+
     private HttpURLConnection httpURLConnection;
+
     private String downloadPaht;
+
     private Proxy proxy;
 
+    private String[] uploadFiles;
+
+    private HashMap<String, String> datas;
+
+    private String fileTageName = "file";
+
+    private String stringDatas;
+
+
     /**
-     * @param url 请求的URL
+     * 请求默认为GET方式，没有回调
+     *
+     * @param url
      */
     public HttpUtils(String url) {
         this(url, null, null, null);
     }
 
     /**
-     * @param url             请求的URl
-     * @param hashMap_headers 协议头
+     * 请求默认为GET方式，有回调
+     *
+     * @param url           url
+     * @param iHttpListener IHttpListener回调接口
      */
-    public HttpUtils(String url, HashMap<String, String> hashMap_headers) {
-        this(url, null, hashMap_headers, null);
+    public HttpUtils(String url, IHttpListener iHttpListener) {
+        this(url, null, null, iHttpListener);
     }
 
     /**
-     * @param url             请求的URl
-     * @param hashMap_headers 协议头
-     * @param iHttpListener   回调接口
-     */
-    public HttpUtils(String url, HashMap<String, String> hashMap_headers, IHttpListener iHttpListener) {
-        this(url, null, hashMap_headers, iHttpListener);
-    }
-
-    /**
-     * @param url  请求的URl
-     * @param data post数据
+     * 请求默认为POST，没有回调
+     *
+     * @param url  url
+     * @param data 数据
      */
     public HttpUtils(String url, String data) {
         this(url, data, null, null);
     }
 
     /**
-     * @param url             请求的URl
-     * @param data            post数据
-     * @param hashMap_headers 协议头
+     * 请求默认为POST，有回调
+     *
+     * @param url           url
+     * @param data          数据
+     * @param iHttpListener IHttpListener回调接口
      */
-    public HttpUtils(String url, String data, HashMap<String, String> hashMap_headers) {
-        this(url, data, hashMap_headers, null);
+    public HttpUtils(String url, String data, IHttpListener iHttpListener) {
+        this(url, data, null, iHttpListener);
+    }
+
+
+    /**
+     * 请求默认为GET需设置协议头HashMap，没有回调
+     *
+     * @param url     url
+     * @param headers HashMap协议头
+     */
+    public HttpUtils(String url, HashMap<String, String> headers) {
+        this(url, null, headers, null);
     }
 
     /**
-     * @param url             请求的URl
-     * @param data            post数据
-     * @param hashMap_headers 协议头
-     * @param iHttpListener   回调接口
+     * 请求默认为GET需设置协议头HashMap，有回调
+     *
+     * @param url           url
+     * @param headers       HashMap协议头
+     * @param iHttpListener IHttpListener回调接口
+     */
+    public HttpUtils(String url, HashMap<String, String> headers, IHttpListener iHttpListener) {
+        this(url, null, headers, iHttpListener);
+    }
+
+    /**
+     * 请求默认为POST需设置协议头HashMap，没有回调
+     *
+     * @param url     url
+     * @param data    请求数据
+     * @param headers 请求协议头
+     */
+    public HttpUtils(String url, String data, HashMap<String, String> headers) {
+        this(url, data, headers, null);
+    }
+
+    /**
+     * @param url             url
+     * @param data            请求数据
+     * @param hashMap_headers 请求协议头
+     * @param iHttpListener   请求回调
      */
     public HttpUtils(String url, String data, HashMap<String, String> hashMap_headers, IHttpListener iHttpListener) {
         this.url = url;
-        if (data != null)
+        if (data != null) {
             this.data = data.getBytes();
+            this.stringDatas = data;
+            this.requestMode = REQUEST_POST;
+        }
+
         if (hashMap_headers == null)
             this.hashMap_headers = new HashMap<>();
         else
@@ -115,6 +187,7 @@ public class HttpUtils {
      */
     public HttpUtils setHeader(String key, String value) {
         this.hashMap_headers.put(key, value);
+
         return this;
     }
 
@@ -132,6 +205,85 @@ public class HttpUtils {
         }
         return this;
     }
+
+    /**
+     * 设置协议头Referer
+     *
+     * @param referer referer
+     * @return HttpUtils
+     */
+    public HttpUtils setReferer(String referer) {
+        if (referer != null && referer != "") {
+            hashMap_headers.put("Referer", referer);
+        }
+        return this;
+    }
+
+    /**
+     * 设置协议头Origin
+     *
+     * @param origin origin
+     * @return HttpUtils
+     */
+    public HttpUtils setOrigin(String origin) {
+        if (origin != null && origin != "") {
+            hashMap_headers.put("Origin", origin);
+        }
+        return this;
+    }
+
+    /**
+     * 设置协议头ContentType
+     *
+     * @param contentType contentType
+     * @return HttpUtils
+     */
+    public HttpUtils setContentType(String contentType) {
+        if (contentType != null && contentType != "") {
+            hashMap_headers.put("Content-Type", contentType);
+        }
+        return this;
+    }
+
+    /**
+     * 设置协议头ContentLength
+     *
+     * @param contentLength contentLength
+     * @return HttpUtils
+     */
+    public HttpUtils setContentLength(String contentLength) {
+        if (contentLength != null && contentLength != "") {
+            hashMap_headers.put("Content-Length", contentLength);
+        }
+        return this;
+    }
+
+    /**
+     * 设置协议头AcceptEncoding
+     *
+     * @param acceptEncoding acceptEncoding
+     * @return HttpUtils
+     */
+    public HttpUtils setAcceptEncoding(String acceptEncoding) {
+        if (acceptEncoding != null && acceptEncoding != "") {
+            hashMap_headers.put("Accept-Encoding", acceptEncoding);
+        }
+        return this;
+    }
+
+    /**
+     * 设置协议头Accept
+     *
+     * @param aceept aceept
+     * @return HttpUtils
+     */
+    public HttpUtils setAceept(String aceept) {
+        if (aceept != null && aceept != "") {
+            hashMap_headers.put("Accept", aceept);
+        }
+        return this;
+    }
+
 
     /**
      * 设置是否重定向
@@ -184,18 +336,96 @@ public class HttpUtils {
      * @return HtppUtils
      */
     public HttpUtils setRawData(byte[] data) {
+        this.requestMode = REQUEST_POST | REQUEST_RAW;
         this.data = data;
         return this;
     }
 
     /**
-     * 设置下载目录
+     * 设置下载目录并自动转为下载模式
      *
-     * @param path 设置了下载目录后将进行下载模式
+     * @param path          设置了下载目录后将进行下载模式
+     * @param requestMethod 设置下载的请求方式GET或POST
      * @return HtppUtils
      */
-    public HttpUtils setDownloadPath(String path) {
+    public HttpUtils setDownloadPath(String path, String requestMethod) {
+        if (this.data != null) {
+            this.requestMode = this.requestMode | REQUEST_DOWNLOAD;
+        } else if (requestMethod.toUpperCase().equals("GET")) {
+            this.requestMode = REQUEST_GET | REQUEST_DOWNLOAD;
+        } else if (requestMethod.toUpperCase().equals("POST")) {
+            this.requestMode = REQUEST_POST | REQUEST_DOWNLOAD;
+        }
         this.downloadPaht = path;
+        return this;
+    }
+
+    /**
+     * 设置下载目录并自动转为下载模式，默认下载请求方式为GET
+     *
+     * @param path
+     * @return HttpUtils
+     */
+    public HttpUtils setDownloadPath(String path) {
+        this.setDownloadPath(path, "GET");
+        return this;
+    }
+
+    /**
+     * 设置要上传的文件路径数组，设置后会自动转换为上传文件模式
+     *
+     * @param datas 上传时请求的数据
+     * @param paths 文件路径数组
+     * @return HttpUtils
+     */
+    public HttpUtils setUploadFiles(HashMap<String, String> datas, String... paths) {
+        this.requestMode = REQUEST_POST | REQUEST_UPLOAD;
+        this.datas = datas;
+        this.uploadFiles = paths;
+        return this;
+    }
+
+
+    /**
+     * 设置要上传的文件路径数组，设置后会自动转换为上传文件模式
+     *
+     * @param fileTageName 文件标签名并不是文件名
+     * @param paths        文件路径数组
+     * @return HttpUtils
+     * @
+     */
+    public HttpUtils setUploadTFiles(String fileTageName, HashMap<String, String> datas, String... paths) {
+        this.fileTageName = fileTageName;
+        this.requestMode = REQUEST_POST | REQUEST_UPLOAD;
+        this.datas = datas;
+        this.uploadFiles = paths;
+        return this;
+    }
+
+    /**
+     * 设置要上传的文件路径数组，设置后会自动转换为上传文件模式
+     *
+     * @param fileTageName 文件标签名并不是文件名
+     * @param paths        文件路径数组
+     * @return HttpUtils
+     */
+    public HttpUtils setUploadTFiles(String fileTageName, String... paths) {
+        this.fileTageName = fileTageName;
+        this.requestMode = this.requestMode | REQUEST_UPLOAD;
+        this.uploadFiles = paths;
+        return this;
+    }
+
+
+    /**
+     * 设置要上传的文件路径数组，设置后会自动转换为上传文件模式
+     *
+     * @param paths 文件路径数组
+     * @return HttpUtils
+     */
+    public HttpUtils setUploadFiles(String... paths) {
+        this.requestMode = this.requestMode | REQUEST_UPLOAD;
+        this.uploadFiles = paths;
         return this;
     }
 
@@ -219,15 +449,56 @@ public class HttpUtils {
                 this.httpURLConnection = (HttpURLConnection) new URL(this.url).openConnection(this.proxy);
             this.httpURLConnection.setConnectTimeout(this.timeout);
             this.httpURLConnection.setDoInput(true);
-
-            if (this.data != null) {
-                this.httpURLConnection.setDoOutput(true);
+            this.httpURLConnection.setDoOutput(true);
+            if (this.requestMode == REQUEST_POST | this.requestMode == (REQUEST_RAW | REQUEST_POST)) {
                 this.httpURLConnection.setRequestMethod("POST");
                 OutputStream outputStream = httpURLConnection.getOutputStream();
                 outputStream.write(this.data);
                 outputStream.flush();
                 outputStream.close();
-            } else {
+            } else if (this.requestMode == (REQUEST_POST | REQUEST_UPLOAD)) {
+                this.httpURLConnection.setRequestMethod("POST");
+                this.httpURLConnection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + REQUEST_LINE);
+                OutputStream outputStream = this.httpURLConnection.getOutputStream();
+//                OutputStream outputStream = new FileOutputStream("C:\\Users\\Jamiexu\\Desktop\\12.txt");
+                PrintWriter printWriter = new PrintWriter(outputStream, true);
+
+                Matcher matcher = Pattern.compile("([^=])=([^&=]+)").matcher(this.stringDatas);
+                while (matcher.find()) {
+                    printWriter.println("--" + REQUEST_LINE);
+                    printWriter.println("Content-Disposition: form-data; name=\"" + matcher.group(1) + "\"");
+                    printWriter.println();
+                    printWriter.println(matcher.group(2));
+                }
+
+                if (this.datas != null)
+                    for (Map.Entry<String, String> m : this.datas.entrySet()) {
+                        printWriter.println("--" + REQUEST_LINE);
+                        printWriter.println("Content-Disposition: form-data; name=\"" + m.getKey() + "\"");
+                        printWriter.println();
+                        printWriter.println(m.getValue());
+                    }
+
+                for (String p : this.uploadFiles) {
+                    printWriter.println("--" + REQUEST_LINE);
+                    printWriter.println("Content-Disposition: form-data; name=\"" + this.fileTageName + "\"; filename=\"" + FileUtils.getFileName(p) + "\"");
+                    printWriter.println();
+                    outputStream.write(Objects.requireNonNull(FileUtils.readFile(p)));
+                }
+                printWriter.write("\r\n--" + REQUEST_LINE + "--");
+                printWriter.flush();
+                printWriter.close();
+                outputStream.flush();
+                outputStream.close();
+            } else if (this.requestMode == (REQUEST_POST | REQUEST_DOWNLOAD)) {
+                this.httpURLConnection.setRequestMethod("POST");
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(this.data);
+                outputStream.flush();
+                outputStream.close();
+            } else if (this.requestMode == (REQUEST_GET | REQUEST_DOWNLOAD)) {
+                this.httpURLConnection.setRequestMethod("GET");
+            } else if (this.requestMode == REQUEST_GET) {
                 this.httpURLConnection.setRequestMethod("GET");
             }
 
@@ -266,7 +537,9 @@ public class HttpUtils {
 
 
                 inputStream = this.httpURLConnection.getInputStream();
-                if (this.downloadPaht != null) {
+                int req = this.requestMode & ~REQUEST_POST;
+                req = req & ~REQUEST_GET;
+                if (req == REQUEST_DOWNLOAD) {
                     fileOutputStream = new FileOutputStream(this.downloadPaht);
                     int len = -1;
                     byte[] byt = new byte[8 * 1024];
@@ -319,6 +592,7 @@ public class HttpUtils {
 
     /**
      * 开始执行可以设置是否异步请求
+     *
      * @param async
      */
     public void execute(boolean async) {
@@ -332,7 +606,7 @@ public class HttpUtils {
     /**
      * 默认采用异步方式进行请求
      */
-    public void execute(){
+    public void execute() {
         this.execute(true);
     }
 
